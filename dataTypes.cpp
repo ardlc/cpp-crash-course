7f87f8a7e69282b5847fcabc6c8df958815d5635
Alex Russell <alexrusselldlc@gmail.com>
2021-12-22 21:07:19 +0000

    data types

---
+++ dataTypes.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // int variable
+    int age = 25;
+    age = 26;
+
+    // int constant
+    const int constAge = 25;
+
+    // float
+    float weight = 65.5;
+
+    // double
+    double balance = 12345.67;
+
+    // character
+    char gender = 'm';
+
+    // string
+    string name = "Alex";
+
+    // boolean
+    bool isTodaySunny = false;
+
+    // array
+    string colours[] = {"red", "green", "pink", "blue", "black"};
+    string days[7] = {};
+    cout << colours[0];
+
+    // take input
+    cin >> days[0];
+    cout << days[0];
+}
